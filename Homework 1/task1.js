b4f94628543e4589afbbf9e137ee28d5aada8629
Rhea-Roxy <79743359+Rhea-Roxy@users.noreply.github.com>
2022-09-13 22:19:40 -0400

    Create task1.js

---
+++ Homework 1/task1.js
@@ -0,0 +1,25 @@
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+
+const camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 500);
+camera.position.set(0, 0, 100);
+camera.lookAt(0, 0, 0);
+
+var scene = new THREE.Scene();
+scene.background = new THREE.Color(0x999999)
+const material = new THREE.LineBasicMaterial({
+  color: 0xff0000
+});
+const points = [];
+points.push(new THREE.Vector3(-10, 0, 0));
+points.push(new THREE.Vector3(0, 10, 0));
+points.push(new THREE.Vector3(10, 0, 0));
+
+
+const geometry = new THREE.BufferGeometry().setFromPoints(points);
+
+const line = new THREE.Line(geometry, material);
+
+scene.add(line)
+renderer.render(scene, camera);
